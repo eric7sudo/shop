ca772b98a2053db9d8121200fd660ab08d49282a
eric karanja <erickaranja54@gmail.com>
2025-01-23 17:14:37 -0700

    website layout

---
+++ app/(home)/page.tsx
@@ -0,0 +1,7 @@
+export default async function Page() {
+  return (
+    <div className='bg-blue-200'>
+      <h1 className='h1-bold text-center p-10'>Home</h1>
+    </div>
+  )
+}
---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'My Shop'
+export const APP_SLOGAN =
+    process.env.NEXT_PUBLIC_APP_SLOGAN || `where affordability meets style.`
+export const APP_DESCRIPTION =
+    process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+    `An e-commerce platform for fashion enthusiasts built with Next.js and MongoDB.`
